0706be22c4cebf06212da48ae810e0f44eb8660c
rnhub <dayal.rn@gmail.com>
2021-11-25 16:47:45 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import softwareCollection from './softwareData';
+import rating from './img/rating.png'
 
-function App() {
+const softwareType = ['antivirus', 'texteditor', 'videoeditor', 'browser', 'mediaplayer', 'other'];
+
+
+const App = () => {
+  const [movieTye, setsoftwareType] = useState('windows');
+  const [genre, setGenre] = useState('antivirus')
+  const [clickedId, setClickedId] = useState(-1);
+  // Movie type toggeler function
+  const togglesoftwareType = () => {
+    if (movieTye === 'windows') {
+      setsoftwareType('linux')
+    }
+    else {
+      setsoftwareType('windows')
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      {/* toggle software type and button */}
+
+      {movieTye === 'windows' ? <button className='toggleBtn' onClick={togglesoftwareType}>windows</button> : <button className='toggleBtn' onClick={togglesoftwareType}>Linux</button>}
+      <div className="sub-container">
+        <div className="left">
+          {
+            softwareType.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  onClick={() => { setGenre(item); setClickedId(index) }}
+                  className={index === clickedId ? "customButton active" : "customButton"}
+                >
+                  {item}
+                </button>
+              );
+            })
+          }
+        </div>
+        <div className="right">
+          <p>software : </p>
+
+          {
+            movieTye === 'windows' ?
+              <ul>{
+                // console.log(Array.isArray(softwareCollection[genre].windows.software))
+                softwareCollection[genre].windows.software.map((item, index) => {
+                  return (
+                    <li
+                      key={index}
+                    >
+                      {item}
+                      
+                      <span>
+                        <img src={rating} alt={`rating`}/>
+                        &nbsp; {softwareCollection[genre].windows.ratings[index]}
+                      </span>
+                      <p>{softwareCollection[genre].windows.description[index]}</p>
+                    </li>
+                  )
+                })
+
+              }
+
+              </ul> :
+              <ul>{
+                // console.log(Array.isArray(softwareCollection[genre].windows.software))
+                softwareCollection[genre].linux.software.map((item, index) => {
+                  return (
+                    <li
+                      key={index}
+                    >
+                      {item}
+                      <span>
+                        <img src={rating} alt={`rating`}/>
+                        &nbsp; {softwareCollection[genre].linux.ratings[index]}
+                      </span>
+                      <p>{softwareCollection[genre].linux.description[index]}</p>
+                    </li>
+                  )
+                })
+
+              }
+
+              </ul>
+          }
+
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/softwareData.js
@@ -0,0 +1,196 @@
+const softwareCollection = {
+    antivirus: {
+        windows: {
+            software: ['Avast', 'Quick Heal', 'NPAV', 'Macafee', 'avira'],
+            ratings: ['8.4/5', '9.0/10', '8.7/10', '8.8/10', '7.3/10'],
+            description: [
+                'anti virus',
+
+                'anti virus',
+
+                'anti virus',
+
+                'anti virus',
+
+                'anti virus',
+            ]
+        },
+        linux: {
+            software: ['Avast', 'Quick Heal', 'NPAV', 'Macafee', 'avira'],
+            ratings: ['6.5/10', '7.2/10', '5.9/10', '5.9/10', '5.4/10'],
+            description: [
+                'anti virus',
+
+                'anti virus',
+
+                'anti virus',
+
+                'anti virus',
+
+                'anti virus',
+              ]
+        }
+    },
+    texteditor: {
+        windows: {
+            software: ['vscode', 'wordpad', 'msword', 'eclipse', 'notepad'],
+            ratings: ['7.7/10', '6.4/10', '8.0/10', '7.4/10', '7.1/10'],
+            description: [
+                'text editor',
+
+                'text editor',
+
+                'text editor',
+
+                'text editor',
+
+                'text editor',
+              ]
+        },
+        linux: {
+            software: ['vscode', 'wordpad', 'vim', 'eclipse', 'notepad'],
+            ratings: ['4.3/10', '6.2/10', '6.5/10', '6.3/10', '4.5/10', '6.9/10'],
+            description: [
+                'text editor',
+
+                'text editor',
+
+                'text editor',
+
+                'text editor',
+
+                'text editor',
+               ]
+        }
+    },
+    videoeditor: {
+        windows: {
+            software: ['obs studio', 'adobe premiere', 'pinacle studio', 'avid liquid', 'kine master'],
+            ratings: ['6.4/10', '6.8/10', '7.3/10', '7.9/10', '7.6/10'],
+            description: [
+                'video editor',
+
+                'video editor',
+
+                'video editor',
+
+                'video editor',
+
+                'video editor',
+            ]
+        },
+        linux: {
+            software: ['obs studio', 'adobe premiere', 'video cutter', 'kdenlive', 'kine master'],
+            ratings: ['6.4/10', '6.8/10', '7.3/10', '7.9/10', '7.6/10'],
+            description: [
+                'video editor',
+
+                'video editor',
+
+                'video editor',
+
+                'video editor',
+
+                'video editor',
+            ]
+        }
+    },
+    browser: {
+        windows: {
+            software: ['Mozilla Firefox', 'Chrome', 'Brave', 'Safari', 'Opera'],
+            ratings: ['7.5/10', '7.5/10', '5.3/10', '5.9/10', '7.1/10', '7.1/10', '7.6/10', '6.2/10'],
+            description: [
+                'browser',
+
+                'browser',
+
+                'browser',
+
+                'browser',
+
+                'browser',
+            ]
+        },
+        linux: {
+            software: ['Mozilla Firefox', 'Chrome', 'Brave', 'Safari', 'Opera'],
+            ratings: ['7.5/10', '7.5/10', '5.3/10', '5.9/10', '7.1/10', '7.1/10', '7.6/10', '6.2/10'],
+            description: [
+                'browser',
+
+                'browser',
+
+                'browser',
+
+                'browser',
+
+                'browser',
+            ]
+        }
+    },
+    mediaplayer: {
+        windows: {
+            software: ['VLC', 'Windows Media Player', 'pot player', 'Jet Audio', 'KM player'],
+            ratings: ['7.8/10', '7.4/10', '7.8/10', '7.3/10', '7.1/10', '6.9/10'],
+            description: [
+                'player',
+
+                'player',
+
+                'player',
+
+                'player',
+
+                'player',
+            ]
+        },
+        linux: {
+            software: ['VLC', 'pulse audio', 'pot player', 'Jet Audio', 'KM player'],
+            ratings: ['7.8/10', '7.4/10', '7.8/10', '7.3/10', '7.1/10', '6.9/10'],
+            description: [
+                'player',
+
+                'player',
+
+                'player',
+
+                'player',
+
+                'player',
+            ]
+        }
+    },
+    other: {
+        windows: {
+            software: ['driver pack solution', 'Virtual Machine', 'bluestack', 'disk partioner'],
+            ratings: ['7.6/10', '7.7/10', '7.0/10', '7.6/10', '8.6/10', '8.5/10', '7.4/10'],
+            description: [
+                'other',
+
+                'other',
+
+                'other',
+
+                'other',
+
+                'other',
+            ]
+        },
+        linux: {
+            software: ['gparted', 'metasploit', 'reaver', 'burp suite'],
+            ratings: ['7.6/10', '7.7/10', '7.0/10', '7.6/10', '8.6/10', '8.5/10', '7.4/10'],
+            description: [
+                'other',
+
+                'other',
+
+                'other',
+
+                'other',
+
+                'other',
+            ]
+        }
+    }
+}
+
+export default softwareCollection;
